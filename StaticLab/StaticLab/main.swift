bc6f01da38f3bde94152ade4c241d33c34716ae5
mustafa-krgoz <karagozhalitmustafa@gmail.com>
2024-10-23 10:59:40 +0300

    Static examples.

--- StaticLab/StaticLab/main.swift
+++ StaticLab/StaticLab/main.swift
@@ -5,7 +5,37 @@
 //  Created by Mustafa Karagöz on 22.10.2024.
 //
 
+
+
 import Foundation
 
-print("Hello, World!")
+class Personel {
+    
+    var Adi: String
+    var SicilNo: Int
+    static var sonPersonelNo = 0
+    
+    init(ad: String){
+        Adi = ad
+        SicilNo = Personel.SicilHesapla()
+    }
+    
+    static func SicilHesapla()-> Int {
+        
+        sonPersonelNo+=1
+        return sonPersonelNo
+    }
+    
+    func bilgileriYaz() {
+        print("Ad: \(Adi)")
+        print("SicilNo: \(SicilNo)")
+    }
+}
+
+var p1 = Personel(ad: "Mustafa")
+p1.bilgileriYaz()
+
+var p2 = Personel(ad: "Karagöz")
+p2.bilgileriYaz()
+
 
