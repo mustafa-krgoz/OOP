431e7a1910139c0ddb39c4313a0d0122b96584d1
mustafa-krgoz <karagozhalitmustafa@gmail.com>
2024-10-24 10:45:50 +0300

    Inheritance example.

--- Kalitim/Kalitim/main.swift
+++ Kalitim/Kalitim/main.swift
@@ -7,5 +7,56 @@
 
 import Foundation
 
-print("Hello, World!")
+class Kare : Sekil {
+    
+    var kenar : Int
+    
+    init(kenar : Int) {
+        self.kenar = kenar
+        super.init(ks : 4)
+    }
+    
+    func Hesapla() {
+        cevre = 4 * kenar
+    }
+
+}
+
+class Dikdortgen : Sekil {
+    
+    var kisaKenar : Int
+    var uzunKenar : Int
+    
+    init(k: Int , u: Int){
+        kisaKenar = k
+        uzunKenar = u
+        super.init()
+    }
+
+}
+
+class Sekil {
+    
+    var kenarSayisi : Int?
+    var cevre : Int?
+    var alan : Int?
+    
+    init(){
+        
+    }
+    init(ks : Int){
+        kenarSayisi = ks
+    }
+}
+
+var c = Kare(kenar: 5)
+c.Hesapla()
+print(c.cevre!)
+print(c.kenarSayisi!)
+
+var d = Dikdortgen(k: 10, u: 20)
+print(d.kenarSayisi)
+
+
+
 
